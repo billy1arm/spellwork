3977085be63ab9bd8ada105caa9f2964202e9dba
KonctantinG <KonctantinG@f2af48d8-1b27-11df-a558-f1789200f0e9>
2010-02-19 21:51:33 +0000

    -- small fix -- select more data from spell* DataTable

--- SpellWork/SpellWork/Form1.cs
+++ SpellWork/SpellWork/Form1.cs
@@ -18,13 +18,25 @@ namespace SpellWork
         }
 
         private DataTable spellData { get; set; }
+        private DataTable spellDuration { get; set; }
+        private DataTable spellRadius { get; set; }
+        private DataTable spellCastTime { get; set; }
+        private DataTable spellRange { get; set; }
+
         private DataTable tempTable { get; set; }
+
+        private LocalesDBC Locales { get; set; }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             // read dbc to DataTable
             Spell spell = new Spell();
             spellData = spell.SpellData;
+            spellDuration = spell.SpellDuration;
+            spellRadius = spell.SpellRadius;
+            spellCastTime = spell.SpellCastTime;
+            spellRange = spell.SpellRange;
+            Locales = spell.Locales;
 
             foreach (var elem in Enum.GetValues(typeof(ProcFlags)))
             {
@@ -84,12 +96,12 @@ namespace SpellWork
 
             var bTarget1 = _cbTarget1.SelectedIndex == 0 ? false : true;
             var fTarget1 = _cbTarget1.SelectedValue.ToString();
-            
-            IEnumerable<DataRow> query = 
+
+            var query =
                 from spell in spellData.AsEnumerable()
                 // filter
                 where (!bFamilyNames || spell.Field<string>("SpellFamilyName") == fFamilyNames)
-                   // if SpellAura selected
+                    // if SpellAura selected
                    && (!bSpellAura   || spell.Field<string>("EffectApplyAuraName_1") == fSpellAura
                                      || spell.Field<string>("EffectApplyAuraName_2") == fSpellAura
                                      || spell.Field<string>("EffectApplyAuraName_3") == fSpellAura)
@@ -99,20 +111,76 @@ namespace SpellWork
                                      || spell.Field<string>("Effect_3") == fSpellEffect)
 
                    && (!bTarget1     || spell.Field<string>("Targets") == fTarget1)
-                
+
                 select spell;
-            
+
             if (query.Count() == 0) return;
-
+            // in temp datatable from speed
             tempTable = query.CopyToDataTable<DataRow>();
-            
-            _lvSpellList.Items.Clear();
-
-            foreach (DataRow item in tempTable.Select())
+            foreach (var elemrnt in tempTable.Select())
             {
-                _lvSpellList.Items.Add(new ListViewItem(new String[] 
-                { "" + item["ID"], "" + item["SpellName_1"]+"("+item["Rank_1"]+")" }));
+                _lvSpellList.Items.Add(new ListViewItem(new String[] { 
+                "" + elemrnt["ID"], "" + elemrnt["SpellName_"+Locales] + " (" + elemrnt["Rank_"+Locales] + ")" }));
             }
+
+        }
+
+        private String SelectCastTimes(string CastingTimeIndex)
+        {
+            var query =
+                from casttime in spellCastTime.AsEnumerable()
+                where casttime.Field<string>("ID") == CastingTimeIndex
+                select casttime;
+
+            if (query.Count() == 0)
+                return "0, 0, 0";
+
+            var result = query.CopyToDataTable<DataRow>().Select().First();
+            return String.Format("{0}, {1}, {2}", result[0], result[1], result[2]);
+        }
+
+        private String SelectDuration(string DurationIndex)
+        {
+            var query =
+                from duration in spellDuration.AsEnumerable()
+                where duration.Field<string>("ID") == DurationIndex
+                select duration;
+
+            if (query.Count() == 0) 
+                return "0, 0, 0";
+
+            var result = query.CopyToDataTable<DataRow>().Select().First();
+            return String.Format("{0}, {1}, {2}", result[0], result[1], result[2]);
+        }
+
+        private String SelectRange(string DurationIndex)
+        {
+            var query =
+                from rande in spellRange.AsEnumerable()
+                where rande.Field<string>("ID") == DurationIndex
+                select rande;
+
+            if (query.Count() == 0)
+                return "0, 0, 0";
+
+            var result = query.CopyToDataTable<DataRow>().Select().First();
+            return String.Format("{0}, {1}, {2}", result[0], result[1], result[2]);
+        }
+
+        private String SelectRadius(string EffectRadiusIndex_1, string EffectRadiusIndex_2, string EffectRadiusIndex_3)
+        {
+            var query =
+                from radius in spellRadius.AsEnumerable()
+                where radius.Field<string>("ID") == EffectRadiusIndex_1
+                   || radius.Field<string>("ID") == EffectRadiusIndex_2
+                   || radius.Field<string>("ID") == EffectRadiusIndex_3
+                select radius;
+
+            if (query.Count() == 0)
+                return "0, 0, 0";
+
+            var result = query.CopyToDataTable<DataRow>().Select().First();
+            return String.Format("{0}, {1}, {2}", result[1], result[2], result[3]);
         }
 
         private void _lvSpellList_SelectedIndexChanged(object sender, EventArgs e)
@@ -121,28 +189,23 @@ namespace SpellWork
             if (_lvSpellList.SelectedItems.Count > 0)
             {
                 var entry = _lvSpellList.SelectedItems[0].SubItems[0].Text;
-                IEnumerable<DataRow> query = from spell in tempTable.AsEnumerable()
-                                             where spell.Field<string>("ID") == entry
-                                             select spell;
-                DataRow[] dtr = query.CopyToDataTable<DataRow>().Select();
+                var query = from spell in tempTable.AsEnumerable()
+                            where spell.Field<string>("ID") == entry
+                            select spell;
 
-                foreach (var spellInfo in dtr)
-                {
-                    SpellViewInfo(spellInfo);
-                }
+                SpellViewInfo(query.CopyToDataTable<DataRow>().Select().First());
             }
         }
-
 
         void SpellViewInfo(DataRow spellInfo)
         {
             _rtSpellInfo.SelectionColor = Color.Blue;
             
-            _rtSpellInfo.AppendText(String.Format("ID - {0} {1}({2})", spellInfo["ID"], spellInfo["SpellName_1"], spellInfo["Rank_1"]));
-            _rtSpellInfo.AppendText("\r\n------------------------------------------");
-            _rtSpellInfo.AppendText("\r\nDescription: " + spellInfo["Description_1"]);
-            _rtSpellInfo.AppendText("\r\nToolTip: "     + spellInfo["ToolTip_1"]);
-            _rtSpellInfo.AppendText("\r\n------------------------------------------");
+            _rtSpellInfo.AppendText(String.Format("ID - {0} {1} ({2})", spellInfo["ID"], spellInfo["SpellName_"+Locales], spellInfo["Rank_"+Locales]));
+            _rtSpellInfo.AppendText("\r\n==========================================");
+            _rtSpellInfo.AppendText("\r\nDescription: " + spellInfo["Description_"+Locales]);
+            _rtSpellInfo.AppendText("\r\nToolTip: "     + spellInfo["ToolTip_"+Locales]);
+            _rtSpellInfo.AppendText("\r\n==========================================");
             _rtSpellInfo.AppendText(String.Format("\r\nCategory = {0}, SpellIconID = {1}, ActiveIconID = {2}, SpellVisual_1 = {3}, SpellVisual_2 = {4}",
                 spellInfo["Category"], spellInfo["SpellIconID"], spellInfo["ActiveIconID"], spellInfo["SpellVisual_1"], spellInfo["SpellVisual_2"]));
 
