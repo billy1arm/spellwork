ce277cbddd006d078c265057418f67c064a22652
KonctantinG <KonctantinG@f2af48d8-1b27-11df-a558-f1789200f0e9>
2010-02-22 16:48:38 +0000

    -- add enums

---
+++ trunk/SpellWork/SpellWork/SpellFieldsEnums.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpellWork
+{
+    enum SpellFields3
+    {
+        EffectDieSides,
+        EffectBaseDice,
+        EffectDicePerLevel,
+        EffectRealPointsPerLevel,
+        EffectBasePoints,
+        EffectMechanic,
+        EffectApplyAuraName,
+        EffectAmplitude,
+        EffectMultipleValue,
+        EffectChainTargets,
+        EffectItemType,
+        EffectMiscValueA,
+        EffectMiscValueB,
+        EffectTriggerSpell,
+        EffectPointsPerComboPoint,
+        EffectSpellClassMaskA,
+        EffectSpellClassMaskB,
+        EffectSpellClassMaskC,
+        DmgMultiplier
+    };
+
+    enum SpellFields2
+    {
+        Stances,
+        StancesNot,
+        Totem,
+        SpellVisual,
+        TotemCategory
+    };
+  
+    enum SpellFieldBloc1
+    {
+        Dispel,
+        Category,
+        SpellIconID,
+        ActiveIconID,
+        DmgClass,
+        PreventionType,
+        SpellLevel,
+        ManaCostPercentage,
+        StartRecoveryCategory,
+        StartRecoveryTime,
+        MaxTargetLevel,
+        MaxAffectedTargets,
+        StanceBarOrder,
+        MinFactionID,
+        MinReputation,
+        RequiredAuraVision,
+        AreaGroupId,
+        RuneCostID,
+        SpellMissileID,
+        PowerDisplayID,
+    };
+
+    enum SpellFieldBloc2
+    {
+        TargetCreatureType,
+        RequiresSpellFocus,
+        FacingCasterFlags,
+        CasterAuraState,
+        TargetAuraState,
+        CasterAuraStateNot,
+        TargetAuraStateNot,
+        CasterAuraSpell,
+        TargetAuraSpell,
+        ExcludeCasterAuraSpell,
+        ExcludeTargetAuraSpell,
+        RecoveryTime,
+        CategoryRecoveryTime,
+        InterruptFlags,
+        AuraInterruptFlags,
+        ChannelInterruptFlags,
+        ProcFlags,
+        ProcChance,
+        ProcCharges,
+        MaxLevel,
+        BaseLevel
+    };
+
+    enum SpellFieldBloc3
+    {
+        PowerType,
+        ManaCost,
+        ManaCostPerLevel,
+        ManaPerSecond,
+        ManaPerSecondPerLeve,
+        Speed,
+        ModalNextSpell,
+        StackAmount,
+    };
+}
